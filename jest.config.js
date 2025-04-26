76660b737042f304c3bdba4f4f7a625c91b1d579
NipunSandilu <nipunsandilu8@gmail.com>
2025-04-26 12:39:12 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   clearMocks: true,
-  setupFilesAfterEnv: ["regenerator-runtime/runtime"],
-  testPathIgnorePatterns: ["/node_modules/"],
-  testMatch: ["<rootDir>/src/*.test.js"],
+  setupFilesAfterEnv: ['regenerator-runtime/runtime', '@testing-library/jest-dom'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['<rootDir>/src/**/*.test.js'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'mjs', 'cjs'],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
 };
